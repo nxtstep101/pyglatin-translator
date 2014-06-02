7597f977d69ff7aea82594c2d86443c0330840d5
nxtstep101 <nxtstep101@gmail.com>
2014-06-02 00:27:46 -0500

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,12 +1,14 @@
 # Originally from codecademy.com with minor additions from Nxtstep101
 
-pyg = 'ay'
 print 'Python Pyglatin Translator - Codeacademy and Nxtstep101'
-user_input = raw_input('Enter a word:')
-if len(user_input) > 0 and user_input.isalpha():
-    word = user_input.lower()
-    first = word[0]
-    transl = word[1:] + first + pyg
-    print 'Translated: %s' % transl
-else:
-    print 'Please enter a proper word'
+
+while True:
+    pyg = 'ay'
+    user_input = raw_input('Enter a word:')
+    if len(user_input) > 0 and user_input.isalpha():
+        word = user_input.lower()
+        first = word[0]
+        transl = word[1:] + first + pyg
+        print 'Translated: %s' % transl
+    else:
+        print 'Please enter a proper word'
